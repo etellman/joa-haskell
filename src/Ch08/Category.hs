64fea77f8481d575f15969e11e1ae10fc31b5010
Edwin Tellman <etellman@pobox.com>
2023-06-30 20:33:31 -0700

    Rename withdest to withDest

--- src/Ch08/Category.hs
+++ src/Ch08/Category.hs
@@ -4,7 +4,6 @@ module Ch08.Category
     Objects (..),
     SumLabel (..),
     isId,
-    withdest,
     sumIdentity,
   )
 where
@@ -35,7 +34,7 @@ class (Eq o, Objects o) => Morphisms o l where
 
   -- | The morphisms with a particular object as the dest
   morphismsTo :: o -> [Morphism o l]
-  morphismsTo = withdest morphisms
+  morphismsTo = withDest morphisms
 
   -- | Compose two morphisms
   (<.>) :: (Semigroup l) => Morphism o l -> Morphism o l -> Morphism o l
@@ -44,8 +43,8 @@ class (Eq o, Objects o) => Morphisms o l where
     | otherwise = Morphism sg (lf <> lg) df
 
 -- | the morphisms with dest x
-withdest :: (Eq a) => [Morphism a b] -> a -> [Morphism a b]
-withdest xs x = filter ((== x) . dest) xs
+withDest :: (Eq a) => [Morphism a b] -> a -> [Morphism a b]
+withDest xs x = filter ((== x) . dest) xs
 
 -- | determines whether a morphism is an identity
 isId :: (Eq a) => Morphism a b -> Bool
