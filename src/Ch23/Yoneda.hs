b43d574c6e065785017256e2f5f4a81a5715ef37
Edwin Tellman <etellman@pobox.com>
2023-06-30 20:30:40 -0700

    Add Yoneda natural transform
    Natural transform --- work in progress

---
+++ src/Ch23/Yoneda.hs
@@ -0,0 +1,22 @@
+module Ch23.Yoneda
+  (
+  )
+where
+
+import Ch08.Category
+import qualified Ch12.SetCategory as SC
+import Text.Printf
+
+naturalH ::
+  (Semigroup l, Morphisms o l) =>
+  SC.SetObject (Morphism o l) ->
+  Morphism o l ->
+  SC.SetMorphism (Morphism o l) (Morphism o l)
+naturalH x f =
+  let transform = (f <.>)
+      naturalObj (SC.SetObject nm fs ok) =
+        SC.SetObject
+          (printf "natural (%s)" nm)
+          (fmap transform fs)
+          (\g -> ok $ f <.> g)
+   in SC.SetMorphism x "natural" transform (naturalObj x)
